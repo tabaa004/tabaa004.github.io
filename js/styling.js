6108051cae5b2ffd3af87846bbc6009791d0fca6
tarjeiba <tarjeibaerland@gmail.com>
2017-09-12 14:27:22 +0200

    Førsteutkast av øveprøve i rettlinjet bevegelse

---
+++ js/styling.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+    function toggleMe(a){
+	var e=document.getElementById(a);
+	if(!e)return true;
+	if(e.style.display=="none"){
+	    e.style.display="block"
+	}
+	else{
+	    e.style.display="none"
+	}
+	return true;
+    }
+</script>
